fe94c44eccd2ac868c9b780786befa73c679ba06
TNdria <tndria.ingenium@gmail.com>
2025-12-08 17:16:57 +0300

    modification backend

--- el_pirata_api/database/seeders/AdminSeeder.php
+++ el_pirata_api/database/seeders/AdminSeeder.php
@@ -15,8 +15,8 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         Admin::create([
-            'name' => 'Super Admin',
-            'email' => 'harinirnamamyandoniaina@gmail.com',
+            'name' => 'Admin',
+            'email' => 'tndria.ingenium@gmail.com',
             'password' => Hash::make('password123'), // 🔐 change le mot de passe
             'role_id' => 1
         ]);
